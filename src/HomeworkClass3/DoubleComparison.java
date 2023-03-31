b2dae0d67fd50f2eb7065eef75f4f7a167bec3e0
boba166Ghub <boba166@yahoo.com>
2023-03-30 23:25:30 -0400

    homework_class3

---
+++ src/HomeworkClass3/DoubleComparison.java
@@ -0,0 +1,21 @@
+package HomeworkClass3;
+
+public class DoubleComparison {
+    public static void main(String[] args) {
+        double num1=10.23;
+        double num2=9.81;
+
+        if (num1>num2)
+        {
+            System.out.println("Double value is larger than");
+        }
+        else
+        {
+            System.out.println("No text");
+        }
+
+
+    }
+
+
+}
