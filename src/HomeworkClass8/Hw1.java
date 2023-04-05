2c8f49a4e859839147fe7f4159075a057d02b0b2
boba166Ghub <boba166@yahoo.com>
2023-04-05 13:51:32 -0400

    homework_class8

---
+++ src/HomeworkClass8/Hw1.java
@@ -0,0 +1,12 @@
+package HomeworkClass8;
+//Print numbers from 1 to 50 except those that are divisible by 3
+
+public class Hw1 {
+    public static void main(String[] args) {
+        for (int i=1;i<=50;i++){
+            if(i%3!=0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
